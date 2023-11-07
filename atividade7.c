aeadccc9f20707a3cf4ed6ceee1e9bb44cbb8f8e
Gustavo00101 <150172214+Gustavo00101@users.noreply.github.com>
2023-11-07 16:23:28 -0300

    atividade7.c

---
+++ atividade7.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+
+int jogo;
+
+
+
+
+printf ("\n==================================\n");
+printf ("\n (1) NOVO JOGO \n");
+printf ("\n (2) CARREGAR JOGO \n");
+printf ("\n (3) CONFIGURACAO \n");
+printf ("\n===================================\n");
+scanf ("%d",&jogo);
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+
+}
