a13e7ad6db3af65173a42dcc381f579fd2a7c85c
Gustavo00101 <150172214+Gustavo00101@users.noreply.github.com>
2023-11-07 15:40:18 -0300

    atividade1.c

---
+++ atividade.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+int temperatura;
+char resultado;
+
+printf ("insira a temperatura do jogo:");
+scanf ("%d",&temperatura);
+ 
+ 
+if (temperatura > 25 )
+ {
+printf ("O clima é ensolarado");
+ }
+ if (temperatura < 15 )
+ {
+printf ("O clima é chuvoso");
+ }
+ else
+ {
+printf ("o clima esta nublado");
+ }
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
