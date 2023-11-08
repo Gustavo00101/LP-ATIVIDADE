6ed4341bf585d518a9537fc0db00aa9f129c0b2a
Gustavo00101 <150172214+Gustavo00101@users.noreply.github.com>
2023-11-08 09:22:25 -0300

    Atualizar o atividade7.c

--- atividade7.c
+++ atividade7.c
@@ -1,32 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main (){
-
+int main(void) {
 
 int jogo;
+int confi;
+
+  printf ("\n (1) NOVO JOGO\n");
+printf ("\n (2)  CARREGAR JOGO\n");
+  printf ("\n(3) configuração\n");
+  scanf("%d",&jogo);
+
+switch ( jogo ) {
+case 1:
+printf("CARREGANDO NOVO JOGO............");
+break;
+case 2:
+printf("CARREGANDO JOGO SALVO");
+break;
+case 3:
+printf("\nDIFICULDADE DO JOGO\n");
+printf("\n(1) MUITO DIFÍCIL\n");
+printf("\n(2)MEDIO\n");
+printf("\n(3) FÁCIL\n");
+scanf("%d",&confi);
+break;
+}
 
 
-
-
-printf ("\n==================================\n");
-printf ("\n (1) NOVO JOGO \n");
-printf ("\n (2) CARREGAR JOGO \n");
-printf ("\n (3) CONFIGURACAO \n");
-printf ("\n===================================\n");
-scanf ("%d",&jogo);
-
-
-
-
-
-
-
-
-
-
-
-
-    return 0;
-
+  return 0;
 }
